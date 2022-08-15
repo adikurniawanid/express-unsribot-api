ee2cde2a2baa693f07bd9f0cea280ec60181c023
Adi Kurniawan <adikurniawan.dev@gmail.com>
2022-08-15 22:47:11 +0700

    minor: change directory wordlist

--- public/index.js
+++ public/index.js
@@ -1,11 +1,11 @@
-const kataPerintah = require("./kataPerintah.json");
-const kondisi = require("./kondisi.json");
-const penangananNamaKolom = require("./penangananNamaKolom.json");
-const penangananNamaTabel = require("./penangananNamaTabel.json");
-const sinonim = require("./sinonim.json");
-const stopword = require("./stopword.json");
-const stemmer = require("./stemmer.json");
-const perbandingan = require("./perbandingan.json");
+const kataPerintah = require("./wordlist/kataPerintah.json");
+const kondisi = require("./wordlist/kondisi.json");
+const penangananNamaKolom = require("./wordlist/penangananNamaKolom.json");
+const penangananNamaTabel = require("./wordlist/penangananNamaTabel.json");
+const sinonim = require("./wordlist/sinonim.json");
+const stopword = require("./wordlist/stopword.json");
+const stemmer = require("./wordlist/stemmer.json");
+const perbandingan = require("./wordlist/perbandingan.json");
 
 module.exports = {
   kataPerintah,
