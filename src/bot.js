c5141c8251bf364e565775e345ff92a5095cf401
Adi Kurniawan <adikurniawan.dev@gmail.com>
2022-08-15 15:12:58 +0700

    feat(bot): refactor

--- src/bot.js
+++ src/bot.js
@@ -7,35 +7,12 @@ const bot = new TelegramBot(telegramBotConfig.TELEGRAM_BOT_TOKEN, {
 });
 
 bot.onText(/\/nl2sql (.+)/, async (msg, match) => {
-  const chatId = msg.chat.id;
   try {
-    const resp = (
-      await axios.post(telegramBotConfig.API_URL + "/nl2sql", {
-        sentence: match[1],
-      })
-    ).data.data;
+    const chatId = msg.chat.id;
+    const opts = {
+      reply_to_message_id: msg.message_id,
+    };
 
-    bot
-      .sendMessage(chatId, JSON.stringify(resp, null, 2), {
-        reply_to_message_id: msg.message_id,
-      })
-      .catch((e) =>
-        bot.sendMessage(chatId, "Bad Request: reply message is too long")
-      );
-  } catch (error) {
-    if (error.response.data.message) {
-      bot.sendMessage(chatId, error.response.data.message);
-    }
-  }
-});
-
-bot.onText(/\/querySingle (.+)/, async (msg, match) => {
-  const chatId = msg.chat.id;
-  const opts = {
-    reply_to_message_id: msg.message_id,
-  };
-
-  try {
     const result = (
       await axios.post(telegramBotConfig.API_URL + "/nl2sql", {
         sentence: match[1],
@@ -43,27 +20,29 @@ bot.onText(/\/querySingle (.+)/, async (msg, match) => {
     ).data.data;
 
     const resp = {
-      sql: result.translator,
-      result: result.resultQuery,
+      sql: result.translate,
+      result: result.query,
     };
 
-    await bot.sendMessage(chatId, resp.sql, opts);
+    await bot.sendMessage(chatId, resp.sql);
     await bot.sendMessage(chatId, "Jumlah data : " + resp.result.length, opts);
 
     resp.result.map((element) => {
       bot
         .sendMessage(
           chatId,
-          JSON.stringify(element, null, 1).replace(/[{}]/g, ""),
-          opts
+          JSON.stringify(element, null, 1).replace(/[{}]/g, "")
         )
         .catch((e) =>
           bot.sendMessage(chatId, "Bad Request: reply message is too long")
         );
     });
   } catch (error) {
+    console.log(error);
     if (error.response.data.message) {
       bot.sendMessage(chatId, error.response.data.message);
+    } else {
+      bot.sendMessage(chatId, error);
     }
   }
 });
