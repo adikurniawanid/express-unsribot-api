5048b20dd5526e5bf384c1472f9d803055e58a9f
Adi Kurniawan <adikurniawan.dev@gmail.com>
2022-08-24 22:07:45 +0700

    feat: bot add more command

--- src/bot.js
+++ src/bot.js
@@ -45,6 +45,86 @@ bot.onText(/\/nl2sql (.+)/, async (msg, match) => {
     console.log(error);
     if (error.response.data.message) {
       bot.sendMessage(chatId, error.response.data.message, opts);
+    } else if (error.response.body.description) {
+      bot.sendMessage(chatId, error.response.body.description, opts);
+    } else {
+      bot.sendMessage(chatId, error);
+    }
+  }
+});
+
+bot.onText(/\/pre (.+)/, async (msg, match) => {
+  const chatId = msg.chat.id;
+  const opts = {
+    reply_to_message_id: msg.message_id,
+  };
+
+  try {
+    const resp = (
+      await axios.post(telegramBotConfig.API_URL + "/nl2sql", {
+        sentence: match[1],
+      })
+    ).data.data.preprocess;
+
+    await bot.sendMessage(chatId, JSON.stringify(resp), opts);
+  } catch (error) {
+    console.log(error);
+    if (error.response.data.message) {
+      bot.sendMessage(chatId, error.response.data.message, opts);
+    } else if (error.response.body.description) {
+      bot.sendMessage(chatId, error.response.body.description, opts);
+    } else {
+      bot.sendMessage(chatId, error);
+    }
+  }
+});
+
+bot.onText(/\/parser (.+)/, async (msg, match) => {
+  const chatId = msg.chat.id;
+  const opts = {
+    reply_to_message_id: msg.message_id,
+  };
+
+  try {
+    const resp = (
+      await axios.post(telegramBotConfig.API_URL + "/nl2sql", {
+        sentence: match[1],
+      })
+    ).data.data.parse;
+
+    await bot.sendMessage(chatId, JSON.stringify(resp, null, 2), opts);
+  } catch (error) {
+    console.log(error);
+    if (error.response.data.message) {
+      bot.sendMessage(chatId, error.response.data.message, opts);
+    } else if (error.response.body.description) {
+      bot.sendMessage(chatId, error.response.body.description, opts);
+    } else {
+      bot.sendMessage(chatId, error);
+    }
+  }
+});
+
+bot.onText(/\/translator (.+)/, async (msg, match) => {
+  const chatId = msg.chat.id;
+  const opts = {
+    reply_to_message_id: msg.message_id,
+  };
+
+  try {
+    const resp = (
+      await axios.post(telegramBotConfig.API_URL + "/nl2sql", {
+        sentence: match[1],
+      })
+    ).data.data.translate;
+
+    await bot.sendMessage(chatId, JSON.stringify(resp), opts);
+  } catch (error) {
+    console.log(error);
+    if (error.response.data.message) {
+      bot.sendMessage(chatId, error.response.data.message, opts);
+    } else if (error.response.body.description) {
+      bot.sendMessage(chatId, error.response.body.description, opts);
     } else {
       bot.sendMessage(chatId, error);
     }
