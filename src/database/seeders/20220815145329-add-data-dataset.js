9f4b7b0f155ffca7df690298b9e6f4f479f11f1d
Adi Kurniawan <adikurniawan.dev@gmail.com>
2022-09-04 16:36:52 +0700

    feat: update seeder and migration for dataset nl

--- src/database/seeders/20220815145329-add-data-dataset.js
+++ src/database/seeders/20220815145329-add-data-dataset.js
@@ -3,10 +3,10 @@ const datasetSeederData = require("../../../public/seederData/dataset.json");
 
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.bulkInsert("t_dataset", datasetSeederData, {});
+    await queryInterface.bulkInsert("t_dataset_nl", datasetSeederData, {});
   },
 
   async down(queryInterface, Sequelize) {
-    await queryInterface.bulkDelete("t_dataset", null, {});
+    await queryInterface.bulkDelete("t_dataset_nl", null, {});
   },
 };
