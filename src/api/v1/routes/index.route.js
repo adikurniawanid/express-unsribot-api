077455d3736203f6fd63925c9ca80fc025d673e8
Adi Kurniawan <adikurniawan.dev@gmail.com>
2022-10-05 09:48:54 +0700

    minor: update welcome page

--- src/api/v1/routes/index.route.js
+++ src/api/v1/routes/index.route.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const router = express.Router();
+var packageInfo = require("../../../../package.json");
 
 router.get("/", function (req, res, next) {
   res.status(200).json({
-    message: "Welcome to the UNSRIBOT API",
+    message: "UNSRIBOT API",
+    version: packageInfo.version,
   });
 });
 
